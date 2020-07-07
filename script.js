db10005265f301a85d8e355562faa164927c95b3
zeeshannawaz160 <64968084+zeeshannawaz160@users.noreply.github.com>
2020-07-08 04:44:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-//document.getElementById('header__top--bars').onclick = 
-
-/*var x = document.querySelector('#header__top--btn');
-console.log(x);
-x.addEventListener('click', function() {
-    document.querySelector('.navigation').style.width = "100px";
-});*/
-
-/*document.getElementById("header__top--btn").onclick = () => {
-    document.getElementsById("navigation").style.width = "100px";
-}*/
 
 document.getElementById("header__top--btn").onclick = function() {myFunction()};        
         function myFunction() {
-            var el = document.querySelector('.navigation');
-            el.style.height = "100vh";
+            document.querySelector('.navigation').style.height = "100vh";
             document.body.style.backgroundColor = "grey";
         }
         document.getElementById("navigation__page--btn").onclick = function() {myFun()};        
         function myFun() {
-            var el = document.querySelector('.navigation');
-            el.style.height = "0";
+            document.querySelector('.navigation').style.height = "0";
             document.body.style.backgroundColor = "white";
         }
 
@@ -31,12 +18,12 @@ document.getElementById("header__top--btn").onclick = function() {myFunction()};
             el2.style.paddingBottom = "100px";
         }*/
 
-        document.querySelector('.navigation__page--item-1').addEventListener('click', function() {
+        /*document.querySelector('.navigation__page--item-1').addEventListener('click', function() {
             var el = document.querySelector('.navigation__page--item-1-add').textContent;
 
             if(el === 'keyboard_arrow_down') {
                 document.querySelector('.navigation__page--item-inner').style.height = "15rem";
-                document.querySelector('.navigation__page--item-1').style.marginBottom = "15rem";
+                document.querySelector('.navigation__page--item-1').style.marginBottom = document.querySelector('.navigation__page--item-inner').style.height;
                 document.querySelector('.navigation__page--item-1-add').textContent = "keyboard_arrow_up";
             }else if (el === 'keyboard_arrow_up') {
                 document.querySelector('.navigation__page--item-inner').style.height = 0;
@@ -44,5 +31,23 @@ document.getElementById("header__top--btn").onclick = function() {myFunction()};
                 document.querySelector('.navigation__page--item-1-add').textContent = "keyboard_arrow_down";
             }
             
-        });
+        });*/
+
+
+        /*var coll = document.getElementsByClassName("navigation__page--item");
+        var coll2 = document.getElementsByClassName("navigation__page--item-inner");
+        var i;
+        var j;
+        var x = new Array();
+
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                for(j = 1; j < 6; j++) {
+                    x = document.getElementsByClassName("navigation__page--item-i");
+                }
+                x[i-1].style.marginBottom = coll2[i].scrollHeight + "px";
+            });
+        } */
+
+
 
